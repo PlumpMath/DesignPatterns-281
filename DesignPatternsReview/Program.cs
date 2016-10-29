62f4414c7b11377a2a831452adb9b6122053c33a
ngracia <nestor.gracia@gmail.com>
2016-10-29 14:02:49 +0100

    Silly things have been removed.

--- DesignPatternsReview/Program.cs
+++ DesignPatternsReview/Program.cs
@@ -1,21 +1,9 @@
-﻿using System;
-using Ninject;
-
-namespace DesignPatternsReview
+﻿namespace DesignPatternsReview
 {
     class Program
     {
         static void Main(string[] args)
-        {   
-            /*
-            Ninject.IKernel kernel = new StandardKernel();
-            kernel.Bind<IFlybehavior>().To<FlyWithWing>();
-            var instance = kernel.Get<Duck>();
-            instance.PerformanceFly();*/
-            
-            Duck duck = new Duck(new FlyWithWing());
-            duck.PerformanceFly();
-            Console.ReadLine();
+        {
         }
     }
 }
